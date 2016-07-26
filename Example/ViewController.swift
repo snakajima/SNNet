2f2f3dc9a2463be88b7b2610da8ea35476b9d26e
tokorom <tokorom@gmail.com>
2016-07-26 12:06:10 +0900

    Add Example.xcodeproj

---
+++ Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SNNet
+//
+//  Created by tokorom on 7/26/16.
+//  Copyright © 2016 swipe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
